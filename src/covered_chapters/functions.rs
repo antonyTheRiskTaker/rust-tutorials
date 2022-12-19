d6ec42bbd03cac1a3ddb6b464c094613df5300b9
antonyTheRiskTaker <mrmikadox@gmail.com>
2022-12-19 12:31:25 +0800

    functions chapter covered;

---
+++ src/covered_chapters/functions.rs
@@ -0,0 +1,30 @@
+// fn test() {
+//     println!("Test has been called...");
+// }
+
+// The "->" construct is Rust's return operator/keyword.
+fn add_numbers(x: i32, y: i32) -> i32 {
+    // println!("The sum is: {}", x + y);
+    // (Line below) simply write an expression you'd like to return from a function. Pls drop the semicolon.
+    // x + y
+    // 10
+    let result = x + y;
+    // return result; // You can also return values like this.
+    if result > 10 {
+        return result - 10;
+    }
+    result
+}
+
+fn main() {
+    println!("Hello, world!"); // A macro is an expression.
+    // add_numbers(20, 30); // A function call is an expression.
+
+    // let number = { // This is an expression because it returns a value.
+    //     let x = 3;
+    //     x + 1
+    // };
+    // println!("{}", number);
+    let result = add_numbers(1, 3);
+    println!("{}", result);
+}
