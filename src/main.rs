fbda06deea44a6319efab4eb6663c7290aaba2f0
antonyTheRiskTaker <mrmikadox@gmail.com>
2022-12-21 21:29:35 +0800

    covered "what is ownership" part

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,17 @@
 fn main() {
-    println!("Hello world!");
+    // let mut s = String::from("hello");
+
+    // s.push_str(", world!"); // push_str() appends a literal to a String
+
+    // println!("{}", s); // This will print `hello, world!`
+
+    // let s1 = String::from("hello");
+    // let s2 = s1;
+
+    // println!("{}, world!", s1); // s1 is invalidated, so it won't compile
+
+    let s1 = String::from("hello");
+    let s2 = s1.clone();
+
+    println!("s1 = {}, s2 = {}", s1, s2);
 }
