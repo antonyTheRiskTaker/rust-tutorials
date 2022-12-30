954502ae9965c1d3710f2b810a1b6b5518cf61fb
antonyTheRiskTaker <mrmikadox@gmail.com>
2022-12-31 02:48:19 +0800

    strings covered; proceeding hashmaps

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,11 @@
-use std::vec;
+// use std::vec;
+use unicode_segmentation::UnicodeSegmentation;
 
-enum SpreadsheetCell {
-    Int(i32),
-    Float(f64),
-    Text(String),
-}
+// enum SpreadsheetCell {
+//     Int(i32),
+//     Float(f64),
+//     Text(String),
+// }
 
 fn main() {
     // let a = [1, 2, 3];
@@ -36,14 +37,51 @@ fn main() {
     //     *i += 50;
     //     println!("{}", i);
     // }
-    let row = vec![
-        SpreadsheetCell::Int(3),
-        SpreadsheetCell::Text(String::from("blue")),
-        SpreadsheetCell::Float(10.12),
-    ];
+    // let row = vec![
+    //     SpreadsheetCell::Int(3),
+    //     SpreadsheetCell::Text(String::from("blue")),
+    //     SpreadsheetCell::Float(10.12),
+    // ];
 
-    match &row[1] {
-        SpreadsheetCell::Int(i) => println!("{}", i),
-        _ => println!("Not an integer!"),
-    };
+    // match &row[1] {
+    //     SpreadsheetCell::Int(i) => println!("{}", i),
+    //     _ => println!("Not an integer!"),
+    // };
+
+    // Strings are stored as a collection of UTF-8 encoded bytes
+    // let s1 = String::new();
+    // let s2 = "initial contents";
+    // let s3 = s2.to_string();
+    // let s4 = String::from("initial contents");
+
+    // One way to append a string
+    // let mut s = String::from("foo");
+    // s.push_str("bar");
+    // s.push('!'); // `s` becomes "foobar!"
+
+    // Another way to append a string
+    // let s1 = String::from("Hello, ");
+    // let s2 = String::from("world!");
+
+    // Moving ownership of `s1` to `s3` and taking all the characters in `s2` and append them to `s3`
+    // let s3 = s1 + &s2;
+
+    // let s3 = format!("{}{}", s1, s2);
+
+    // println!("{}", s1);
+
+    let hello = String::from("नमस्ते");
+
+    // for b in "नमस्ते".bytes() {
+    //     println!("{}", b);
+    // }
+
+    // for c in "नमस्ते".chars() {
+    //     println!("{}", c);
+    // }
+
+    // for g in "नमस्ते".graphemes(true) {
+    //     println!("{}", g);
+    // }
+
 }
