8ddca84148965b417b8de68a79744df253694039
antonyTheRiskTaker <mrmikadox@gmail.com>
2023-01-04 12:11:56 +0800

    covering generic data types until listing 10-4

--- src/main.rs
+++ src/main.rs
@@ -1,24 +1,37 @@
-// fn largest(list: &[i32]) -> &i32 {
-//     let mut largest = &list[0];
+fn largest_i32(list: &[i32]) -> &i32 {
+    let mut largest = &list[0];
 
-//     for item in list {
-//         if item > largest {
-//             largest = item;
-//         }
-//     }
+    for item in list {
+        if item > largest {
+            largest = item;
+        }
+    }
 
-//     largest
-// }
+    largest
+}
+
+fn largest_char(list: &[char]) -> &char {
+    let mut largest = &list[0];
+
+    for item in list {
+        if item > largest {
+            largest = item;
+        }
+    }
+
+    largest
+}
 
 fn main() {
-    // let number_list = vec![34, 50, 25, 100, 65];
+    let number_list = vec![34, 50, 25, 100, 65];
 
-    // let result = largest(&number_list);
-    // println!("The largest number is {result}");
+    let result = largest_i32(&number_list);
+    println!("The largest number is {result}");
 
-    // let number_list = vec![102, 34, 6000, 89, 54, 2, 43, 8];
+    let char_list = vec!['y', 'm', 'a', 'q'];
 
-    // let result = largest(&number_list);
-    // println!("The largest number is {result}");
-    
+    let result = largest_char(&char_list);
+    println!("The largest char is {result}");
+    // TODO: continue from listing 10-4
+
 }
