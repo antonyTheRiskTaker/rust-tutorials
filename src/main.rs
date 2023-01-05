7926bc7c7b4638d90e9a7c8fb998b993c528eed5
antonyTheRiskTaker <mrmikadox@gmail.com>
2023-01-05 11:29:54 +0800

    covering generic data types until 'In Enum Definitions'

--- src/main.rs
+++ src/main.rs
@@ -1,37 +1,71 @@
-fn largest_i32(list: &[i32]) -> &i32 {
-    let mut largest = &list[0];
+// use std;
 
-    for item in list {
-        if item > largest {
-            largest = item;
-        }
-    }
+// fn largest_i32(list: &[i32]) -> &i32 {
+//     let mut largest = &list[0];
 
-    largest
-}
+//     for item in list {
+//         if item > largest {
+//             largest = item;
+//         }
+//     }
 
-fn largest_char(list: &[char]) -> &char {
-    let mut largest = &list[0];
+//     largest
+// }
 
-    for item in list {
-        if item > largest {
-            largest = item;
-        }
-    }
+// fn largest_char(list: &[char]) -> &char {
+//     let mut largest = &list[0];
 
-    largest
-}
+//     for item in list {
+//         if item > largest {
+//             largest = item;
+//         }
+//     }
+
+//     largest
+// }
+
+// fn largest<T: std::cmp::PartialOrd>(list: &[T]) -> &T {
+//     let mut largest = &list[0];
+
+//     for item in list {
+//         if item > largest {
+//             largest = item;
+//         }
+//     }
+
+//     largest
+// }
+
+// struct Point<T> {
+//     x: T,
+//     y: T,
+// }
+
+// struct Point<T, U> {
+//     x: T,
+//     y: U,
+// }
 
 fn main() {
-    let number_list = vec![34, 50, 25, 100, 65];
+    // let number_list = vec![34, 50, 25, 100, 65];
 
-    let result = largest_i32(&number_list);
-    println!("The largest number is {result}");
+    // let result = largest_i32(&number_list);
+    // let result = largest(&number_list);
+    // println!("The largest number is {result}");
 
-    let char_list = vec!['y', 'm', 'a', 'q'];
+    // let char_list = vec!['y', 'm', 'a', 'q'];
 
-    let result = largest_char(&char_list);
-    println!("The largest char is {result}");
-    // TODO: continue from listing 10-4
+    // let result = largest_char(&char_list);
+    // let result = largest(&char_list);
+    // println!("The largest char is {result}");
 
+    // let integer = Point { x: 5, y: 10 };
+    // let float = Point { x: 1.0, y: 4.0 };
+    // let wont_work = Point { x: 5, y: 4.0 };
+
+    // let both_integer = Point { x: 5, y: 10 };
+    // let both_float = Point { x: 1.0, y: 4.0 };
+    // let integer_and_float = Point { x: 5, y: 4.0 };
+
+    // TODO: continue from `In Enum Definition`
 }
