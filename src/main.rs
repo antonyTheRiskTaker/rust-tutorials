e1abdbaddcd2e0aa5343e088e26c5efba01f55e8
antonyTheRiskTaker <mrmikadox@gmail.com>
2023-01-18 12:58:36 +0800

    removed code to library for better organisation
    public api exposed so that the binary crate can access and use it

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,5 @@
 use std::env;
-use std::fs;
 use std::process;
-use std::error::Error;
 
 fn main() {
     let args: Vec<String> = env::args().collect();
@@ -21,30 +19,4 @@ fn main() {
         process::exit(1);
     }
 
-}
-
-fn run(config: Config) -> Result<(), Box<dyn Error>> {
-    let contents = fs::read_to_string(config.file_path)?;
-
-    println!("With text:\n{contents}");
-
-    Ok(())
-}
-
-struct Config {
-    query: String,
-    file_path: String,
-}
-
-impl Config {
-    fn build(args: &[String]) -> Result<Config, &'static str> {
-        if args.len() < 3 {
-            return Err("not enough arguments");
-        }
-
-        let query = args[1].clone();
-        let file_path = args[2].clone();
-
-        Ok(Config { query, file_path })
-    }
 }
