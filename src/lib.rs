877193581234fa6ac8c93492270846aeb8a33b6b
antonyTheRiskTaker <mrmikadox@gmail.com>
2023-01-20 15:43:31 +0800

    environment variables section covered

--- src/lib.rs
+++ src/lib.rs
@@ -1,9 +1,14 @@
-use std::error::Error;
-use std::fs;
+use std::{
+    error::Error,
+    fs,
+    env,
+};
+
 
 pub struct Config {
     pub query: String,
     pub file_path: String,
+    pub ignore_case: bool,
 }
 
 impl Config {
@@ -15,14 +20,27 @@ impl Config {
         let query = args[1].clone();
         let file_path = args[2].clone();
 
-        Ok(Config { query, file_path })
+        let ignore_case = env::var("IGNORE_CASE")
+            .is_ok();
+
+        Ok(Config { 
+            query, 
+            file_path,
+            ignore_case,
+        })
     }
 }
 
 pub fn run(config: Config) -> Result<(), Box<dyn Error>> {
     let contents = fs::read_to_string(config.file_path)?;
 
-    for line in search(&config.query, &contents) {
+    let results = if config.ignore_case {
+        search_case_insensitive(&config.query, &contents)
+    } else {
+        search(&config.query, &contents)
+    };
+
+    for line in results {
         println!("{line}");
     }
 
@@ -45,7 +63,16 @@ pub fn search_case_insensitive<'a>(
     query: &str, 
     contents: &'a str,
 ) -> Vec<&'a str> {
-    vec![]
+    let query = query.to_lowercase();
+    let mut results = Vec::new();
+
+    for line in contents.lines() {
+        if line.to_lowercase().contains(&query) {
+            results.push(line);
+        }
+    }
+
+    results
 }
 
 #[cfg(test)]
