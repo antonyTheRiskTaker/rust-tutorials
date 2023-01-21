1d75da9675c0e851c686b2578e7f1e74e69ec608
antonyTheRiskTaker <mrmikadox@gmail.com>
2023-01-21 10:31:59 +0800

    textbook cli project finished

--- src/main.rs
+++ src/main.rs
@@ -8,12 +8,12 @@ fn main() {
 
     let config = Config::build(&args)
         .unwrap_or_else(|err| {
-            println!("Problem parsing arguments: {err}");
+            eprintln!("Problem parsing arguments: {err}");
             process::exit(1);
         });
 
     if let Err(e) = rust_tutorials::run(config) {
-        println!("Application error: {e}");
+        eprintln!("Application error: {e}");
         process::exit(1);
     }
 
