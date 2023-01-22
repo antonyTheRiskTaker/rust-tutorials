1bcd445b6840e15ed0c742c88d17ea92c593728e
antonyTheRiskTaker <mrmikadox@gmail.com>
2023-01-22 18:46:57 +0800

    covered moving-ownership-references section

--- src/main.rs
+++ src/main.rs
@@ -1,52 +1,75 @@
-#[derive(Debug, PartialEq, Copy, Clone)]
-enum ShirtColor {
-    Red,
-    Blue,
-}
+// #[derive(Debug, PartialEq, Copy, Clone)]
+// enum ShirtColor {
+//     Red,
+//     Blue,
+// }
 
-struct Inventory {
-    shirts: Vec<ShirtColor>,
-}
+// struct Inventory {
+//     shirts: Vec<ShirtColor>,
+// }
 
-impl Inventory {
-    fn giveaway(&self, user_preference: Option<ShirtColor>) -> ShirtColor {
-        user_preference.unwrap_or_else(|| self.most_stocked())
-    }
+// impl Inventory {
+//     fn giveaway(&self, user_preference: Option<ShirtColor>) -> ShirtColor {
+//         user_preference.unwrap_or_else(|| self.most_stocked())
+//     }
 
-    fn most_stocked(&self) -> ShirtColor {
-        let mut num_red = 0;
-        let mut num_blue = 0;
+//     fn most_stocked(&self) -> ShirtColor {
+//         let mut num_red = 0;
+//         let mut num_blue = 0;
 
-        for color in &self.shirts {
-            match color {
-                ShirtColor::Red => num_red += 1,
-                ShirtColor::Blue => num_blue += 1,
-            }
-        }
-        if num_red > num_blue {
-            ShirtColor::Red
-        } else {
-            ShirtColor::Blue
-        }
-    }
-}
+//         for color in &self.shirts {
+//             match color {
+//                 ShirtColor::Red => num_red += 1,
+//                 ShirtColor::Blue => num_blue += 1,
+//             }
+//         }
+//         if num_red > num_blue {
+//             ShirtColor::Red
+//         } else {
+//             ShirtColor::Blue
+//         }
+//     }
+// }
+
+use std::thread;
 
 fn main() {
-    let store = Inventory {
-        shirts: vec![ShirtColor::Blue, ShirtColor::Red, ShirtColor::Blue],
-    };
+    // let store = Inventory {
+    //     shirts: vec![ShirtColor::Blue, ShirtColor::Red, ShirtColor::Blue],
+    // };
 
-    let user_pref1 = Some(ShirtColor::Red);
-    let giveaway1 = store.giveaway(user_pref1);
-    println!(
-        "The user with preference {:?} gets {:?}",
-        user_pref1, giveaway1
-    );
+    // let user_pref1 = Some(ShirtColor::Red);
+    // let giveaway1 = store.giveaway(user_pref1);
+    // println!(
+    //     "The user with preference {:?} gets {:?}",
+    //     user_pref1, giveaway1
+    // );
 
-    let user_pref2 = None;
-    let giveaway2 = store.giveaway(user_pref2);
-    println!(
-        "The user with preference {:?} gets {:?}",
-        user_pref2, giveaway2
-    );
+    // let user_pref2 = None;
+    // let giveaway2 = store.giveaway(user_pref2);
+    // println!(
+    //     "The user with preference {:?} gets {:?}",
+    //     user_pref2, giveaway2
+    // );
+
+    // let example_closure = |x| x;
+
+    // let s = example_closure(String::from("hello"));
+    // let n = example_closure(5);
+
+    // * Capturing References or Moving Ownership
+    let list = vec![1, 2, 3];
+    println!("Before defining closure: {:?}", list);
+
+    thread::spawn(move || println!("From thread: {:?}", list))
+        .join()
+        .unwrap();
+
+    // let only_borrows = || println!("From closure: {:?}", list);
+    // let mut borrows_mutably = || list.push(7);
+
+    // println!("Before calling closure: {:?}", list);
+    // only_borrows();
+    // borrows_mutably();
+    // println!("After calling closure: {:?}", list);
 }
