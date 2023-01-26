f67ba004cc793fc2e0d1638bd34dcd5768aad7f3
antonyTheRiskTaker <mrmikadox@gmail.com>
2023-01-26 12:14:28 +0800

    covered until figure 14.1

---
+++ src/lib.rs
@@ -0,0 +1,13 @@
+/// Adds one to the number given.
+/// 
+/// # Examples
+/// 
+/// ```
+/// let arg = 5;
+/// let answer = rust_tutorials::add_one(arg);
+/// 
+/// assert_eq!(6, answer);
+/// ```
+pub fn add_on(x: i32) -> i32 {
+    x + 1
+}
