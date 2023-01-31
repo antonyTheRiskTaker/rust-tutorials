d00069fc4bcf031868935fec7ecdad5765630f73
antonyTheRiskTaker <mrmikadox@gmail.com>
2023-01-31 10:09:28 +0800

    covered until Listing 15-6

--- src/main.rs
+++ src/main.rs
@@ -4,19 +4,25 @@
 //     Nil,
 // }
 
-enum List {
-    Cons(i32, Box<List>),
-    Nil,
-}
+// enum List {
+//     Cons(i32, Box<List>),
+//     Nil,
+// }
 
-use crate::List::{Cons, Nil};
+// use crate::List::{Cons, Nil};
 
 fn main() {
     // let b = Box::new(5);
     // println!("b = {b}");
 
     // let list = Cons(1, Cons(2, Cons(3, Nil))); // Error
-    let list = 
-        Cons(1, Box::new(Cons(2, Box::new(Cons(3, Box::new(Nil))))));
+    // let list = 
+        // Cons(1, Box::new(Cons(2, Box::new(Cons(3, Box::new(Nil))))));
+    
+    let x = 5;
+    let y = &x;
 
+    assert_eq!(5, x);
+    assert_eq!(5, *y);
+    // TODO: continue from `Using Box<T> Like a Reference`
 }
