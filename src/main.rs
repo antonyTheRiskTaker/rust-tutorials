b0aca3ad68b787986f29ced31733e5444f2f7967
antonyTheRiskTaker <mrmikadox@gmail.com>
2023-02-01 11:21:17 +0800

    new section of ch15 covered
    until 'Implicit Deref Coersions with Functions and Methods'

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,5 @@
+use std::ops::Deref;
+
 // Error! Without using Box<T> for this recursive type
 // enum List {
 //     Cons(i32, List),
@@ -11,6 +13,24 @@
 
 // use crate::List::{Cons, Nil};
 
+// Implement the Deref trait for user-defined MyBox<T>
+impl<T> Deref for MyBox<T> {
+    type Target = T;
+
+    fn deref(&self) -> &Self::Target {
+        &self.0
+    }
+}
+
+// Defining our own smart pointer
+struct MyBox<T>(T);
+
+impl <T> MyBox<T> {
+    fn new(x: T) -> MyBox<T> {
+        MyBox(x)
+    }
+}
+
 fn main() {
     // let b = Box::new(5);
     // println!("b = {b}");
@@ -19,10 +39,19 @@ fn main() {
     // let list = 
         // Cons(1, Box::new(Cons(2, Box::new(Cons(3, Box::new(Nil))))));
     
+    // Using reference
+    // let x = 5;
+    // let y = &x;
+
+    // Using a Box<T> that behaves like a reference
+    // let x = 5;
+    // let y= Box::new(x);
+
+    // Using user-defined MyBox<T>
     let x = 5;
-    let y = &x;
+    let y = MyBox::new(x);
 
     assert_eq!(5, x);
     assert_eq!(5, *y);
-    // TODO: continue from `Using Box<T> Like a Reference`
+    
 }
