51224402705bfaea4e68c7f2a5d416d3f8c6d23a
antonyTheRiskTaker <mrmikadox@gmail.com>
2023-02-02 11:28:17 +0800

    ch15.2 covered

--- src/main.rs
+++ src/main.rs
@@ -48,10 +48,17 @@ fn main() {
     // let y= Box::new(x);
 
     // Using user-defined MyBox<T>
-    let x = 5;
-    let y = MyBox::new(x);
+    // let x = 5;
+    // let y = MyBox::new(x);
 
-    assert_eq!(5, x);
-    assert_eq!(5, *y);
+    // assert_eq!(5, x);
+    // assert_eq!(5, *y);
+
+    let m = MyBox::new(String::from("Rust"));
+    hello(&m);
     
+}
+
+fn hello(name: &str) {
+    println!("Hello, {name}!");
 }
