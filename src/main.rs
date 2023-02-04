180fca7aa9a030c351157e330cdb6c1df90610de
antonyTheRiskTaker <mrmikadox@gmail.com>
2023-02-04 11:56:57 +0800

    commented out unnecessary code

--- src/main.rs
+++ src/main.rs
@@ -13,13 +13,13 @@
 // }
 
 // List using Rc<T>
-enum List {
-    Cons(i32, Rc<List>),
-    Nil,
-}
+// enum List {
+//     Cons(i32, Rc<List>),
+//     Nil,
+// }
 
-use crate::List::{Cons, Nil};
-use std::rc::Rc;
+// use crate::List::{Cons, Nil};
+// use std::rc::Rc;
 
 // Implement the Deref trait for user-defined MyBox<T>
 // impl<T> Deref for MyBox<T> {
@@ -97,15 +97,15 @@ fn main() {
     // let b = Cons(3, Box::new(a));
     // let c = Cons(4, Box::new(a));
 
-    let a = Rc::new(Cons(5, Rc::new(Cons(10, Rc::new(Nil)))));
-    println!("count after creating a = {}", Rc::strong_count(&a));
-    let b = Cons(3, Rc::clone(&a));
-    println!("count after creating b = {}", Rc::strong_count(&a));
-    {
-        let c = Cons(4, Rc::clone(&a));
-        println!("count after creating c = {}", Rc::strong_count(&a));
-    }
-    println!("count after c goes out of scope = {}", Rc::strong_count(&a));
+    // let a = Rc::new(Cons(5, Rc::new(Cons(10, Rc::new(Nil)))));
+    // println!("count after creating a = {}", Rc::strong_count(&a));
+    // let b = Cons(3, Rc::clone(&a));
+    // println!("count after creating b = {}", Rc::strong_count(&a));
+    // {
+    //     let c = Cons(4, Rc::clone(&a));
+    //     println!("count after creating c = {}", Rc::strong_count(&a));
+    // }
+    // println!("count after c goes out of scope = {}", Rc::strong_count(&a));
     
 }
 
