53e25fdfdcddf2c8a63e11dbcd8ead37c9c94328
antonyTheRiskTaker <mrmikadox@gmail.com>
2023-02-04 11:48:10 +0800

    covered ch15.4

--- src/main.rs
+++ src/main.rs
@@ -6,12 +6,20 @@
 //     Nil,
 // }
 
+// List using Box<T>
 // enum List {
 //     Cons(i32, Box<List>),
 //     Nil,
 // }
 
-// use crate::List::{Cons, Nil};
+// List using Rc<T>
+enum List {
+    Cons(i32, Rc<List>),
+    Nil,
+}
+
+use crate::List::{Cons, Nil};
+use std::rc::Rc;
 
 // Implement the Deref trait for user-defined MyBox<T>
 // impl<T> Deref for MyBox<T> {
@@ -80,6 +88,24 @@ fn main() {
     // println!("CustomSmartPointers created.");
     // drop(c);
     // println!("CustomSmartPointer dropped before the end of main.");
+
+    // Using Rc<T> to Share Data
+
+    // We are not allowed to have two lists using Box<T> that try to share
+    // ownership of a third list
+    // let a = Cons(5, Box::new(Cons(10, Box::new(Nil))));
+    // let b = Cons(3, Box::new(a));
+    // let c = Cons(4, Box::new(a));
+
+    let a = Rc::new(Cons(5, Rc::new(Cons(10, Rc::new(Nil)))));
+    println!("count after creating a = {}", Rc::strong_count(&a));
+    let b = Cons(3, Rc::clone(&a));
+    println!("count after creating b = {}", Rc::strong_count(&a));
+    {
+        let c = Cons(4, Rc::clone(&a));
+        println!("count after creating c = {}", Rc::strong_count(&a));
+    }
+    println!("count after c goes out of scope = {}", Rc::strong_count(&a));
     
 }
 
