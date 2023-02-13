08d0e1c236d15e0e558d6657b8e18b128e857da0
antonyTheRiskTaker <mrmikadox@gmail.com>
2023-02-13 22:50:01 +0800

    cover 'Visualizing Changes to strong_count and weak_count'

--- src/main.rs
+++ src/main.rs
@@ -26,11 +26,12 @@
 
 // use crate::List::{Cons, Nil};
 use std::cell::RefCell;
-use std::rc::Rc;
+use std::rc::{Rc, Weak};
 
 #[derive(Debug)]
 struct Node {
     value: i32,
+    parent: RefCell<Weak<Node>>,
     children: RefCell<Vec<Rc<Node>>>,
 }
 
@@ -179,15 +180,22 @@ fn main() {
 
     let leaf = Rc::new(Node {
         value: 3,
+        parent: RefCell::new(Weak::new()),
         children: RefCell::new(vec![]),
     });
 
+    println!("leaf parent = {:?}", leaf.parent.borrow().upgrade());
+
     let branch = Rc::new(Node {
         value: 5,
+        parent: RefCell::new(Weak::new()),
         children: RefCell::new(vec![Rc::clone(&leaf)]),
     });
 
-    // TODO: continue from `Adding a Reference from a Child to Its Parent`
+    *leaf.parent.borrow_mut() = Rc::downgrade(&branch);
+
+    println!("leaf parent = {:?}", leaf.parent.borrow().upgrade());
+    
 }
 
 // fn hello(name: &str) {
