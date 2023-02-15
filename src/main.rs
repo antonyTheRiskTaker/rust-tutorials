48d166ee8785ccd35f48bfe3e9e2782bd55e3049
antonyTheRiskTaker <mrmikadox@gmail.com>
2023-02-15 10:35:25 +0800

    cover until 'Using move Closures with Thread'

--- src/main.rs
+++ src/main.rs
@@ -2,17 +2,19 @@ use std::thread;
 use std::time::Duration;
 
 fn main() {
-    thread::spawn(|| {
+    let handle = thread::spawn(|| {
         for i in 1..10 {
             println!("hi number {i} from the spawned thread!");
             thread::sleep(Duration::from_millis(1));
         }
     });
+    
+    handle.join().unwrap();
 
     for i in 1..5 {
         println!("hi number {i} from the main thread!");
         thread::sleep(Duration::from_millis(1));
     }
 
-    // TODO: continue from `Waiting for All Threads to Finish Using join Handles`
+    // TODO: continue from `Using move Closures with Threads`
 }
