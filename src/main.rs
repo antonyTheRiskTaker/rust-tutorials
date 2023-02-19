6b9a8a5dc6f4f9e0218b22f45c59f738d04d56fe
antonyTheRiskTaker <mrmikadox@gmail.com>
2023-02-19 11:07:04 +0800

    cover ch.16.2

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,5 @@
 use std::thread;
-// use std::time::Duration;
+use std::time::Duration;
 use std::sync::mpsc;
 
 fn main() {
@@ -29,11 +29,41 @@ fn main() {
 
     let (tx, rx) = mpsc::channel();
 
+    let tx1 = tx.clone();
     thread::spawn(move || {
-        let val = String::from("hi");
-        tx.send(val).unwrap();
+        // let val = String::from("hi");
+        // tx.send(val).unwrap();
+        // println!("val is {}", val); // Error!
+        let vals = vec![
+            String::from("hi"),
+            String::from("from"),
+            String::from("the"),
+            String::from("thread"),
+        ];
+
+        for val in vals {
+            tx1.send(val).unwrap();
+            thread::sleep(Duration::from_secs(1));
+        }
     });
 
-    let received = rx.recv().unwrap();
-    println!("Got: {}", received);
+    thread::spawn(move || {
+        let vals = vec![
+            String::from("more"),
+            String::from("messages"),
+            String::from("for"),
+            String::from("you"),
+        ];
+
+        for val in vals {
+            tx.send(val).unwrap();
+            thread::sleep(Duration::from_secs(1));
+        }
+    });
+
+    // let received = rx.recv().unwrap();
+    // println!("Got: {}", received);
+    for received in rx {
+        println!("Got: {received}");
+    }
 }
