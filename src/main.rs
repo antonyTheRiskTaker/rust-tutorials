89afff93340f1c6d3fef745d49f4b6a846085826
antonyTheRiskTaker <mrmikadox@gmail.com>
2023-02-23 22:18:15 +0800

    cover all til 'Sharing a Mutex<T> Between Mutiple Threads'

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,7 @@
-use std::thread;
-use std::time::Duration;
-use std::sync::mpsc;
+// use std::thread;
+// use std::time::Duration;
+// use std::sync::mpsc;
+use std::sync::Mutex;
 
 fn main() {
     // let handle = thread::spawn(|| {
@@ -27,43 +28,54 @@ fn main() {
 
     // handle.join().unwrap();
 
-    let (tx, rx) = mpsc::channel();
+    // let (tx, rx) = mpsc::channel();
 
-    let tx1 = tx.clone();
-    thread::spawn(move || {
+    // let tx1 = tx.clone();
+    // thread::spawn(move || {
         // let val = String::from("hi");
         // tx.send(val).unwrap();
         // println!("val is {}", val); // Error!
-        let vals = vec![
-            String::from("hi"),
-            String::from("from"),
-            String::from("the"),
-            String::from("thread"),
-        ];
+        // let vals = vec![
+        //     String::from("hi"),
+        //     String::from("from"),
+        //     String::from("the"),
+        //     String::from("thread"),
+        // ];
 
-        for val in vals {
-            tx1.send(val).unwrap();
-            thread::sleep(Duration::from_secs(1));
-        }
-    });
+        // for val in vals {
+        //     tx1.send(val).unwrap();
+        //     thread::sleep(Duration::from_secs(1));
+        // }
+    // });
 
-    thread::spawn(move || {
-        let vals = vec![
-            String::from("more"),
-            String::from("messages"),
-            String::from("for"),
-            String::from("you"),
-        ];
+    // thread::spawn(move || {
+    //     let vals = vec![
+    //         String::from("more"),
+    //         String::from("messages"),
+    //         String::from("for"),
+    //         String::from("you"),
+    //     ];
 
-        for val in vals {
-            tx.send(val).unwrap();
-            thread::sleep(Duration::from_secs(1));
-        }
-    });
+    //     for val in vals {
+    //         tx.send(val).unwrap();
+    //         thread::sleep(Duration::from_secs(1));
+    //     }
+    // });
 
     // let received = rx.recv().unwrap();
     // println!("Got: {}", received);
-    for received in rx {
-        println!("Got: {received}");
+    // for received in rx {
+    //     println!("Got: {received}");
+    // }
+
+    let m = Mutex::new(5);
+
+    {
+        let mut num = m.lock().unwrap();
+        *num = 6;
     }
+
+    println!("m = {:?}", m);
+    
+    // TODO: continue from `Sharing a Mutex<T> Between Multiple Threads`
 }
