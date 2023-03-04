5f4bd3678fee3386010f7ed57ff87c226d8d5a03
antonyTheRiskTaker <mrmikadox@gmail.com>
2023-03-05 00:30:48 +0800

    cover til `Storing the Text of the Post Content`

--- src/lib.rs
+++ src/lib.rs
@@ -57,3 +57,23 @@ impl Draw for Button {
         // code to actually draw a button
     }
 }
+
+pub struct Post {
+    state: Option<Box<dyn State>>,
+    content: String,
+}
+
+impl Post {
+    pub fn new() -> Post {
+        Post {
+            state: Some(Box::new(Draft {})),
+            content: String::new(),
+        }
+    }
+}
+
+trait State {}
+
+struct Draft {}
+
+impl State for Draft {}
