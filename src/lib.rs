01ea8c726a30822b57cf5de15fd2f49995a446d4
antonyTheRiskTaker <mrmikadox@gmail.com>
2023-03-06 23:03:21 +0800

    cover til `Requesting a Review of the Post Changes Its State`

--- src/lib.rs
+++ src/lib.rs
@@ -74,6 +74,10 @@ impl Post {
     pub fn add_text(&mut self, text: &str) {
         self.content.push_str(text);
     }
+
+    pub fn content(&self) -> &str {
+        ""
+    }
 }
 
 trait State {}
