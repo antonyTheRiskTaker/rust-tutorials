72fdb33cf68b83a3c9f2108c2250176d6f1f0f4f
antonyTheRiskTaker <mrmikadox@gmail.com>
2023-03-10 00:13:01 +0800

    cover til `Implementing Transitions as Transformation into Different Types`

--- src/lib.rs
+++ src/lib.rs
@@ -59,38 +59,51 @@ impl Draw for Button {
 }
 
 pub struct Post {
-    state: Option<Box<dyn State>>,
+    // state: Option<Box<dyn State>>,
+    content: String,
+}
+
+pub struct DraftPost {
     content: String,
 }
 
 impl Post {
-    pub fn new() -> Post {
-        Post {
-            state: Some(Box::new(Draft {})),
+    pub fn new() -> DraftPost {
+        DraftPost {
+            // state: Some(Box::new(Draft {})),
             content: String::new(),
         }
     }
 
+    // pub fn add_text(&mut self, text: &str) {
+    //     self.content.push_str(text);
+    // }
+
+    pub fn content(&self) -> &str {
+        // self.state.as_ref().unwrap().content(self)
+        &self.content
+    }
+
+    // pub fn request_review(&mut self) {
+    //     if let Some(s) = self.state.take() {
+    //         self.state = Some(s.request_review())
+    //     }
+    // }
+
+    // pub fn approve(&mut self) {
+    //     if let Some(s) = self.state.take() {
+    //         self.state = Some(s.approve())
+    //     }
+    // }
+}
+
+impl DraftPost {
     pub fn add_text(&mut self, text: &str) {
         self.content.push_str(text);
     }
-
-    pub fn content(&self) -> &str {
-        self.state.as_ref().unwrap().content(self)
-    }
-
-    pub fn request_review(&mut self) {
-        if let Some(s) = self.state.take() {
-            self.state = Some(s.request_review())
-        }
-    }
-
-    pub fn approve(&mut self) {
-        if let Some(s) = self.state.take() {
-            self.state = Some(s.approve())
-        }
-    }
 }
+
+// TODO: continue from here
 
 trait State {
     fn request_review(self: Box<Self>) -> Box<dyn State>;
