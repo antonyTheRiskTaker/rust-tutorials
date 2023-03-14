38c42cb053d287673b48a76e8c107775eeb86935
antonyTheRiskTaker <mrmikadox@gmail.com>
2023-03-14 20:44:27 +0800

    cover til `let Statements`

--- src/main.rs
+++ src/main.rs
@@ -17,13 +17,19 @@ fn main() {
     //     println!("Using blue as the background color");
     // }
 
-    let mut stack = Vec::new();
+    // let mut stack = Vec::new();
 
-    stack.push(1);
-    stack.push(2);
-    stack.push(3);
+    // stack.push(1);
+    // stack.push(2);
+    // stack.push(3);
 
-    while let Some(top) = stack.pop() {
-        println!("{top}");
+    // while let Some(top) = stack.pop() {
+    //     println!("{top}");
+    // }
+
+    let v = vec!['a', 'b', 'c'];
+
+    for (index, value) in v.iter().enumerate() {
+        println!("{value} is at index {index}");
     }
 }
