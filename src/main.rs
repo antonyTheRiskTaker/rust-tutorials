12a15b929cb9415a7e4321a18d8dc50a9b166e3f
antonyTheRiskTaker <mrmikadox@gmail.com>
2023-03-19 23:50:02 +0800

    cover til `Multiple Patterns`

--- src/main.rs
+++ src/main.rs
@@ -60,7 +60,16 @@ fn main() {
     //     _ => println!("anything"),
     // }
 
-    // TODO: continue from `Matching Named Variables`
+    let x = Some(5);
+    let y = 10;
+
+    match x {
+        Some(50) => println!("Got 50"),
+        Some(y) => println!("Match, y = {y}"),
+        _ => println!("Default case, x = {:?}", x),
+    }
+
+    println!("at the end: x = {:?}, y = {y}", x);
 }
 
 // fn print_coordinates(&(x, y): &(i32, i32)) {
