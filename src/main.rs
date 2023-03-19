11b3ddc1adfb8bd13a764322a681edfb735e2be2
antonyTheRiskTaker <mrmikadox@gmail.com>
2023-03-19 23:34:45 +0800

    comment out unused code

--- src/main.rs
+++ src/main.rs
@@ -50,6 +50,17 @@ fn main() {
     // if let x = 5 {
     //     println!("{x}");
     // }
+
+    // let x = 1;
+
+    // match x {
+    //     1 => println!("one"),
+    //     2 => println!("two"),
+    //     3 => println!("three"),
+    //     _ => println!("anything"),
+    // }
+
+    // TODO: continue from `Matching Named Variables`
 }
 
 // fn print_coordinates(&(x, y): &(i32, i32)) {
