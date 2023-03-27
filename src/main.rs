4c1c905fedf8dcd2a032257186d3d1ad3bca49a6
antonyTheRiskTaker <mrmikadox@gmail.com>
2023-03-27 21:22:58 +0800

    cover til `Destructuring Nested Structs and Enums`

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,13 @@
-struct Point {
-    x: i32,
-    y: i32,
+// struct Point {
+//     x: i32,
+//     y: i32,
+// }
+
+enum Message {
+    Quit,
+    Move { x: i32, y: i32 },
+    Write(String),
+    ChangeColor(i32, i32, i32),
 }
 
 fn main() {
@@ -99,7 +106,7 @@ fn main() {
     //     _ => println!("something else"),
     // }
 
-    let p = Point { x: 0, y: 7 };
+    // let p = Point { x: 0, y: 7 };
 
     // let Point { x: a, y: b } = p;
     // assert_eq!(0, a);
@@ -109,11 +116,29 @@ fn main() {
     // assert_eq!(0, x);
     // assert_eq!(7, y);
 
-    match p {
-        Point { x, y: 0 } => println!("On the x axis at {x}"),
-        Point { x: 0, y } => println!("On the y axis at {y}"),
-        Point { x, y } => {
-            println!("On neither axis: ({x}, {y})");
+    // match p {
+    //     Point { x, y: 0 } => println!("On the x axis at {x}"),
+    //     Point { x: 0, y } => println!("On the y axis at {y}"),
+    //     Point { x, y } => {
+    //         println!("On neither axis: ({x}, {y})");
+    //     }
+    // }
+
+    let msg = Message::ChangeColor(0, 160, 255);
+    // let msg = Message::Quit;
+
+    match msg {
+        Message::Quit => {
+            println!("The Quit variant has no data to destructure.");
+        }
+        Message::Move { x, y } => {
+            println!("Move in the x direction {x} and in the y direction {y}");
+        }
+        Message::Write(text) => {
+            println!("Text message: {text}");
+        }
+        Message::ChangeColor(r, g, b) => {
+            println!("Change the color to red {r}, green {g}, and blue {b}");
         }
     }
 }
