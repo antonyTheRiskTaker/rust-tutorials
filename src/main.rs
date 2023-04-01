9635bc665b978938867731f8ae857071348dbe55
antonyTheRiskTaker <mrmikadox@gmail.com>
2023-04-02 01:37:48 +0800

    cover til `@ Bindings`

--- src/main.rs
+++ src/main.rs
@@ -230,7 +230,32 @@ fn main() {
     //     }
     // }
 
-    
+    // let num = Some(4);
+
+    // match num {
+    //     Some(x) if x % 2 == 0 => println!("The number {x} is even"),
+    //     Some(x) => println!("The number {x} is odd"),
+    //     None => (),
+    // }
+
+    // let x = Some(5);
+    // let y = 10;
+
+    // match x {
+    //     Some(50) => println!("Got 50"),
+    //     Some(n) if n == y => println!("Match, n = {n}"),
+    //     _ => println!("Default case, x = {:?}", x),
+    // }
+
+    // println!("at the end: x = {:?}, y = {y}", x);
+
+    let x = 4;
+    let y = false;
+
+    match x {
+        4 | 5 | 6 if y => println!("yes"),
+        _ => println!("no"),
+    }
 }
 
 // fn print_coordinates(&(x, y): &(i32, i32)) {
