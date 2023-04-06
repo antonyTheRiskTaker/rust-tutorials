62d6cd40f40ea8b70d81f6c7e2b197310c4fb934
antonyTheRiskTaker <mrmikadox@gmail.com>
2023-04-06 12:18:40 +0800

    cover til `Creating a Safe Abstraction over Unsafe Code`

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,22 @@
 fn main() {
-    let mut num = 5;
+    // let mut num = 5;
 
-    let r1 = &num as *const i32;
-    let r2 = &mut num as *mut i32;
+    // let r1 = &num as *const i32;
+    // let r2 = &mut num as *mut i32;
 
     // Creating a raw pointer to an arbitrary memory address
     // let address = 0x012345usize;
     // let r = address as *const i32;
 
     // Dereferencing raw pointers within an `unsafe` block
+    // unsafe {
+    //     println!("r1 is: {}", *r1);
+    //     println!("r2 is: {}", *r2);
+    // }
+
     unsafe {
-        println!("r1 is: {}", *r1);
-        println!("r2 is: {}", *r2);
+        dangerous();
     }
 }
+
+unsafe fn dangerous() {}
