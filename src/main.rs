a67771d17d6e0b7a6e9c0201c053e444df15b1e8
antonyTheRiskTaker <mrmikadox@gmail.com>
2023-04-07 12:51:32 +0800

    cover til `Using extern Functions to Call External Code`

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,5 @@
+use std::slice;
+
 fn main() {
     // let mut num = 5;
 
@@ -14,9 +16,39 @@ fn main() {
     //     println!("r2 is: {}", *r2);
     // }
 
-    unsafe {
-        dangerous();
-    }
+    // unsafe {
+    //     dangerous();
+    // }
+
+    // let mut v = vec![1, 2, 3, 4, 5, 6];
+
+    // let r = &mut v[..];
+
+    // let (a, b) = r.split_at_mut(3);
+
+    // assert_eq!(a, &mut [1, 2, 3]);
+    // assert_eq!(b, &mut [4, 5, 6]);
+
+    // let address = 0x01234usize;
+    // let r = address as *mut i32;
+
+    // let values: &[i32] = unsafe { slice::from_raw_parts_mut(r, 10000) };
 }
 
-unsafe fn dangerous() {}
+// unsafe fn dangerous() {}
+
+// fn split_at_mut(values: &mut [i32], mid: usize) -> (&mut [i32], &mut [i32]) {
+//     let len = values.len();
+//     let ptr = values.as_mut_ptr();
+
+//     assert!(mid <= len);
+
+//     unsafe {
+//         (
+//             slice::from_raw_parts_mut(ptr, mid),
+//             slice::from_raw_parts_mut(ptr.add(mid), len - mid),
+//         )
+//     }
+
+    // (&mut values[..mid], &mut values[mid..])
+// }
