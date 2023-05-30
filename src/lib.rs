a00bff283e56f266f693c6bcd6ba8d21ececf034
antonyTheRiskTaker <mrmikadox@gmail.com>
2023-05-31 07:55:34 +0800

    cover part of a section
    Continue from Listing 20-20

--- src/lib.rs
+++ src/lib.rs
@@ -44,17 +44,21 @@ impl ThreadPool {
     }
 }
 
-// TODO: continue from Listing 20-19
-
 struct Worker {
     id: usize,
     thread: thread::JoinHandle<()>,
 }
 
+// TODO: continue from Listing 20-20
+
 impl Worker {
     fn new(id: usize, receiver: Arc<Mutex<mpsc::Receiver<Job>>>) -> Worker {
-        let thread = thread::spawn(|| {
-            receiver;
+        let thread   = thread::spawn(move || loop {
+            let job = receiver.lock().unwrap().recv().unwrap();
+
+            println!("Worker {id} got a job; executing.");
+            
+            job();
         });
 
         Worker { id, thread }
